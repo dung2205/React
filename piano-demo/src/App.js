7c40b67973e5d4a8c0b8014af28d7774e7f4bb10
dung2205 <66163648+dung2205@users.noreply.github.com>
2020-06-25 21:30:22 +0700

    Add files via upload

---
+++ piano-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Piano from './Components/Piano.js';
+
+function App() {
+  return (
+    <div>
+      <h1 className="header">Play your piano</h1>  
+      <Piano/>
+    </div>
+  );
+}
+
+export default App;
